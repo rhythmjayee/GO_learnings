936d6446576b7ed05adfe0cdc70ef85fafebdb59
rhythmjayee <jayeerythm8@gmail.com>
2022-06-13 15:41:07 +0530

    Why Go

---
+++ Basics.go
@@ -0,0 +1,14 @@
+/*
+Why Go ?
+Python -> easy to use but slow
+Java -> With large system becomes complex
+C/C++ -> Complex + Slow compile time
+
+Go ->
+Strong & Statically typed
+Simple
+Fast compile time
+garbage collected
+Built in concurrency - Better at handle concurrency
+Compile to standalone binaries
+*/
